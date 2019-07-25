a5a510b644ac475d76ec80779e7a9f71d9aa2865
Akin <akin.gan@oocl.com>
2019-07-25 09:19:49 +0800

    feat: add the model class Package and Customer.

---
+++ src/main/java/com/oocl/packagebooking/model/Customer.java
@@ -0,0 +1,20 @@
+package com.oocl.packagebooking.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/com/oocl/packagebooking/model/Package.java
@@ -0,0 +1,77 @@
+package com.oocl.packagebooking.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Package {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "logisticsNumber")
+    private String logisticsNumber;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id", referencedColumnName = "id")
+    private Customer customer;
+
+    @Column(name = "logisticsNumber")
+    private String mobilePhone;
+
+    @Column(name = "status")
+    private int status;
+
+    @Column(name = "appointmentTime")
+    private Date appointmentTime;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getLogisticsNumber() {
+        return logisticsNumber;
+    }
+
+    public void setLogisticsNumber(String logisticsNumber) {
+        this.logisticsNumber = logisticsNumber;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public String getMobilePhone() {
+        return mobilePhone;
+    }
+
+    public void setMobilePhone(String mobilePhone) {
+        this.mobilePhone = mobilePhone;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public Date getAppointmentTime() {
+        return appointmentTime;
+    }
+
+    public void setAppointmentTime(Date appointmentTime) {
+        this.appointmentTime = appointmentTime;
+    }
+}
